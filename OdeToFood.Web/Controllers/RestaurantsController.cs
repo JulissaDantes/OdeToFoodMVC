b7745635e1e808a97f88705f60a80bd6b46cf709
julissadantes <julissadcj@gmail.com>
2019-12-08 18:25:55 -0400

    Creating a Partial class to list restaurants

--- OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -71,6 +71,7 @@ namespace OdeToFood.Web.Controllers
             if (ModelState.IsValid)
             {
                 db.Update(restaurant);
+                TempData["Message"] = "You have edited the restaurant";
                 return RedirectToAction("Details", new { id = restaurant.Id});
             }
             return View(restaurant);
